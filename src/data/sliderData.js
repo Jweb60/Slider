edb509b8a1944480c063e5a2fdbcf58212b4b44a
Jean-Yves <jy.babiarczyk@gmail.com>
2023-07-19 10:04:29 +0200

    Second commit

---
+++ src/data/sliderData.js
@@ -0,0 +1,27 @@
+ const sliderData = [
+  {
+    id: 1,
+    description: "Grand Canyon Ouest"
+  },
+  {
+    id: 2,
+    description: "Route 66"
+  },
+  {
+    id: 3,
+    description: "Resto Mr.D'Z - Route 66"
+  },
+  {
+    id: 4,
+    description: "Delicate Arch"
+  },
+  {
+    id: 5,
+    description: "Fleuve Colorado"
+  },
+  {
+    id: 6,
+    description: "Antelope Canyon"
+  },
+]
+export default sliderData;
